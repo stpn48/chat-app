aa7b6d6abb09938bc328e6bff876d69547320b5f
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-23 19:37:42 +0200

    Create signInWithProvider.ts

---
+++ src/app/actions/signInWithProvider.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+import { Provider } from "@supabase/supabase-js";
+
+export async function signInWithProvider(provider: Provider) {
+  const supabase = createClient();
+
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: provider,
+    options: {
+      redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth`,
+    },
+  });
+
+  if (error) {
+    return { error: error.message, url: null };
+  }
+
+  if (data.url) {
+    return { error: null, url: data.url };
+  }
+
+  return { error: null, url: null };
+}
