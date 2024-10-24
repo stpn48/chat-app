23417b9ad1fbf631a2745aa5efd07baa375e8c48
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-24 20:40:52 +0200

    Create createMessage.ts

---
+++ src/app/actions/createMessage.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import prisma from "@/utils/prisma";
+import { getUser } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+
+export async function createMessage(chatId: string, message: string) {
+  const user = await getUser();
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  try {
+    await prisma.message.create({
+      data: {
+        chatId,
+        userId: user.id,
+        text: message,
+      },
+    });
+
+    revalidatePath("/app");
+  } catch (error: any) {
+    throw new Error(error.message || "Error creating message");
+  }
+}
