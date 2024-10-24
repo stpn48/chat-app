3ceb94fe6a6cc378ef9bc0b5698b0c52734c6ab1
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-24 22:19:36 +0200

    Update server.ts

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -21,3 +21,13 @@ export function createClient() {
     },
   });
 }
+
+export async function getUser() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return user;
+}
