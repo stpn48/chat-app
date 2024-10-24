8766eddea1f42d0b7fec320e877b4bcd6d3ad8e8
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-24 20:40:54 +0200

    Update route.ts

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 // The client you created from the Server-Side Auth instructions
+import prisma from "@/utils/prisma";
 import { createClient } from "@/utils/supabase/server";
 
 export async function GET(request: Request) {
@@ -11,6 +12,29 @@ export async function GET(request: Request) {
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
     if (!error) {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      console.log(user);
+
+      if (user) {
+        await prisma.userProfile.upsert({
+          where: {
+            id: user.id,
+          },
+          create: {
+            id: user.id,
+            username: user.user_metadata.full_name || "",
+            user_metadata: user.user_metadata,
+          },
+          update: {
+            username: user.user_metadata.full_name || "", // Update with username if provided
+            user_metadata: user.user_metadata,
+          },
+        });
+      }
+
       return NextResponse.redirect(`${origin}/app`);
     } else {
       // Redirect with error query param on failure
