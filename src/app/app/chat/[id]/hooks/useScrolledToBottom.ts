37c9c0176219512cf17c0fe16d9bee86b0e7d980
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-25 18:27:44 +0200

    Create useScrolledToBottom.ts

---
+++ src/app/app/chat/[id]/hooks/useScrolledToBottom.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export function useScrolledToBottom(
+  messages: {
+    chatId: string;
+    id: string;
+    createdAt: Date;
+    userId: string;
+    text: string;
+  }[],
+  containerRef: React.RefObject<HTMLDivElement>,
+) {
+  const [isScrolledToBottom, setIsScrolledToBottom] = useState(false);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTo({
+        top: containerRef.current.scrollHeight,
+        behavior: "instant",
+      });
+      setIsScrolledToBottom(true);
+    }
+  }, [messages, setIsScrolledToBottom]);
+
+  return { isScrolledToBottom };
+}
