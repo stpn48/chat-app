e20aac516660ab26198612a31f678855fab69e0c
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-25 18:27:49 +0200

    Update UserCard.tsx

--- src/app/components/Sidebar/UsersMenu/UserCard.tsx
+++ src/app/components/Sidebar/UsersMenu/UserCard.tsx
@@ -8,16 +8,24 @@ import React, { useCallback } from "react";
 
 type Props = {
   otherUser: UserProfile;
+  closeMenu: () => void;
 };
 
-export function UserCard({ otherUser }: Props) {
+export function UserCard({ otherUser, closeMenu }: Props) {
   const handleClick = useCallback(async () => {
+    closeMenu();
     await createChat(otherUser);
-  }, [otherUser.id]);
+  }, [otherUser.id, closeMenu]);
 
   return (
-    <div onClick={handleClick} className="flex cursor-pointer rounded-lg hover:bg-hover p-2 gap-4 items-center">
-      <Image className="rounded-full" src={(otherUser.user_metadata as UserMetadata).avatar_url} width={32} height={32} alt="avatar" />
+    <div onClick={handleClick} className="flex cursor-pointer items-center gap-4 rounded-lg p-2 hover:bg-hover">
+      <Image
+        className="rounded-full"
+        src={(otherUser.user_metadata as UserMetadata).avatar_url}
+        width={32}
+        height={32}
+        alt="avatar"
+      />
       <h1>{otherUser.username}</h1>
     </div>
   );
