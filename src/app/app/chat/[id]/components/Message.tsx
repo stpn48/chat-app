b6536296cf23a0e5ced5a26c8b27c35e46646ed7
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-25 18:27:43 +0200

    Create Message.tsx

---
+++ src/app/app/chat/[id]/components/Message.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Message as MessageType } from "@prisma/client";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+  msg: MessageType;
+  isAuthor: boolean;
+  isLastMessage: boolean;
+};
+
+export function Message({ msg, isAuthor, isLastMessage }: Props) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div key={msg.chatId} className={twMerge("relative flex w-full", isAuthor && "justify-end")}>
+      <div className="flex flex-col items-end gap-px">
+        <p
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          className={twMerge(
+            "cursor-default rounded-full px-3 py-2 text-white",
+            isAuthor && "bg-blue-600",
+            !isAuthor && "bg-stone-100 text-black",
+          )}
+        >
+          {msg.text}
+        </p>
+
+        {isLastMessage && isAuthor && <p className="text-secondary pr-1 text-xs">{msg.status}</p>}
+      </div>
+
+      <AnimatePresence>
+        {isHovered && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { delay: 0.3 } }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className="absolute -top-10 z-10 rounded-full border border-[#353535] bg-black bg-opacity-90 px-4 py-2 text-white"
+          >
+            {msg.createdAt.toLocaleString()}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+}
