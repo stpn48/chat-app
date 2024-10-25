3eb698b411dd9d2ba2a945448e494c5401c718ae
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-25 18:27:47 +0200

    Update Sidebar.tsx

--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import prisma from "@/utils/prisma";
 import { getUser } from "@/utils/supabase/server";
+import Image from "next/image";
 import React from "react";
 import { ChatList } from "./ChatList";
 import { SignOutButton } from "./SignOutButton";
@@ -34,10 +35,14 @@ export async function Sidebar() {
   console.log(chats);
 
   return (
-    <div className="p-2 h-screen">
-      <div className="w-[325px] relative p-4 border border-main rounded-lg bg-main shadow-md h-full">
-        <div className="flex py-2 justify-between">
-          <h1>Your chats</h1>
+    <div className="h-screen p-2">
+      <div className="relative flex h-full w-[325px] flex-col rounded-lg border border-main bg-main p-4 shadow-md">
+        <div className="mb-5 flex items-center gap-3">
+          <Image className="rounded-full" src={user?.user_metadata.avatar_url} width={32} height={32} alt="avatar" />
+          <h1 className="">{user?.user_metadata.full_name}</h1>
+        </div>
+        <div className="flex justify-between py-2">
+          <h1 className="text-base font-medium">Your chats</h1>
           <StartNewChatButton users={users} />
         </div>
         <ChatList chats={chats} />
