6319f5f29f6d438be8e9d866545c8da3a40e54cc
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-25 18:27:46 +0200

    Update ChatCard.tsx

--- src/app/components/Sidebar/ChatCard.tsx
+++ src/app/components/Sidebar/ChatCard.tsx
@@ -11,9 +11,15 @@ type Props = {
 export function ChatCard({ chat }: Props) {
   return (
     <Link href={`/app/chat/${chat.id}`}>
-      <div key={chat.id} className="flex hover:bg-hover p-2 rounded-lg cursor-pointer  gap-4 items-center">
-        <Image className="rounded-full" src={(chat.users[0].user_metadata as UserMetadata).avatar_url} width={32} height={32} alt="avatar" />
-        <h1>{chat.users[0].username}</h1>
+      <div key={chat.id} className="flex cursor-pointer items-center gap-4 rounded-lg p-2 hover:bg-hover">
+        <Image
+          className="rounded-full"
+          src={(chat.users[1].user_metadata as UserMetadata).avatar_url}
+          width={32}
+          height={32}
+          alt="avatar"
+        />
+        <h1>{chat.users[1].username}</h1>
       </div>
     </Link>
   );
