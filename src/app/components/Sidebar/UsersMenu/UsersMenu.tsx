53a002c745aad1f3c153266a7e98609ec5a046d0
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-25 21:45:42 +0200

    Update UsersMenu.tsx

--- src/app/components/Sidebar/UsersMenu/UsersMenu.tsx
+++ src/app/components/Sidebar/UsersMenu/UsersMenu.tsx
@@ -3,14 +3,16 @@
 import { UserProfile } from "@prisma/client";
 import { motion } from "framer-motion";
 import React from "react";
+import { twMerge } from "tailwind-merge";
 import { UserCard } from "./UserCard";
 
 type Props = {
   otherUsers: UserProfile[];
   closeMenu: () => void;
+  className?: string;
 };
 
-export function UsersMenu({ otherUsers, closeMenu }: Props) {
+export function UsersMenu({ otherUsers, closeMenu, className }: Props) {
   return (
     <>
       <div onClick={closeMenu} className="fixed inset-0 z-10 h-screen w-screen" />
@@ -19,9 +21,12 @@ export function UsersMenu({ otherUsers, closeMenu }: Props) {
         transition={{ duration: 0.1 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        className="absolute right-4 top-12 z-10 flex w-[250px] flex-col gap-2 rounded-lg border bg-main p-4 shadow-lg"
+        className={twMerge(
+          "absolute right-4 z-10 flex w-[250px] flex-col gap-2 rounded-lg border bg-main p-4 shadow-lg",
+          className,
+        )}
       >
-        <p className="text-secondary mb-2 flex w-full justify-center">Select a user to chat with:</p>
+        <p className="mb-2 flex w-full justify-center bg-secondary text-secondary">Select a user to chat with:</p>
         {otherUsers.map((otherUser) => (
           <UserCard closeMenu={closeMenu} key={otherUser.id} otherUser={otherUser} />
         ))}
