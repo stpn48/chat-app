75ebbd7e4b02af5738244104a7261b2dd4b8b490
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-26 21:47:02 +0200

    Update UserCard.tsx

--- src/app/components/Sidebar/UsersMenu/UserCard.tsx
+++ src/app/components/Sidebar/UsersMenu/UserCard.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import { createChat } from "@/app/actions/createChat";
-import { UserMetadata } from "@/types/types";
+import { UserCredentials } from "@/components/UserCredentials";
 import { UserProfile } from "@prisma/client";
-import Image from "next/image";
 import React, { useCallback } from "react";
 
 type Props = {
@@ -18,15 +17,11 @@ export function UserCard({ otherUser, closeMenu }: Props) {
   }, [closeMenu, otherUser]);
 
   return (
-    <div onClick={handleClick} className="flex cursor-pointer items-center gap-4 rounded-lg p-2 hover:bg-hover">
-      <Image
-        className="rounded-full"
-        src={(otherUser.user_metadata as UserMetadata).avatar_url}
-        width={32}
-        height={32}
-        alt="avatar"
-      />
-      <h1>{otherUser.username}</h1>
+    <div
+      onClick={handleClick}
+      className="flex cursor-pointer items-center gap-4 rounded-lg p-2 hover:bg-hover dark:hover:bg-dark-hover"
+    >
+      <UserCredentials user={otherUser} />
     </div>
   );
 }
