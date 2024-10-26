f695fa34a147535b8fceaa6eec6a66bf6ea1407e
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-26 21:46:58 +0200

    Update SettingsMenu.tsx

--- src/app/components/Sidebar/SettingsMenu/SettingsMenu.tsx
+++ src/app/components/Sidebar/SettingsMenu/SettingsMenu.tsx
@@ -2,6 +2,7 @@
 
 import { AnimatePresence, motion } from "framer-motion";
 import React, { useState } from "react";
+import { ChangeThemeButton } from "./ChangeThemeButton";
 import { SettingsButton } from "./SettingsButton";
 import { SignOutButton } from "./SignOutButton";
 
@@ -9,21 +10,28 @@ export function SettingsMenu() {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <div className="relative">
+    <div className="relative flex">
       <AnimatePresence>
         {showMenu && (
           <>
-            <div className="fixed left-0 top-0 z-10 h-screen w-screen" onClick={() => setShowMenu(false)} />
+            <div
+              className="fixed left-0 top-0 z-10 h-screen w-screen whitespace-nowrap"
+              onClick={() => setShowMenu(false)}
+            />
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               transition={{ duration: 0.1 }}
-              className="absolute -top-[200px] z-10 flex w-fit flex-col gap-2 rounded-lg border border-main bg-secondary p-2 shadow-lg"
+              className="absolute -top-[200px] z-10 flex w-fit flex-col gap-2 rounded-lg border border-main bg-main bg-secondary p-2 shadow-lg dark:border-dark-main dark:bg-dark-main"
             >
-              <button className="rounded-lg px-4 py-2 hover:bg-hover">Privacy</button>
-              <button className="rounded-lg px-4 py-2 hover:bg-hover">Change Theme</button>
-              <button className="rounded-lg px-4 py-2 hover:bg-hover">Help</button>
+              <button className="rounded-lg px-4 py-2 hover:bg-hover dark:hover:bg-dark-hover">
+                Privacy
+              </button>
+              <ChangeThemeButton />
+              <button className="rounded-lg px-4 py-2 hover:bg-hover dark:hover:bg-dark-hover">
+                Help
+              </button>
               <SignOutButton />
             </motion.div>
           </>
