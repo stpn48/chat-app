e4f2b2c33c117d1742a7ac5b45df101f06901d80
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-26 21:46:55 +0200

    Update ChatList.tsx

--- src/app/components/Sidebar/ChatList.tsx
+++ src/app/components/Sidebar/ChatList.tsx
@@ -9,7 +9,7 @@ type Props = {
 export function ChatList({ chats }: Props) {
   return (
     <div className="flex flex-col gap-2">
-      <div className="h-px w-full rounded-full bg-hover" />
+      <div className="h-px w-full rounded-full bg-hover dark:bg-[#272727]" />
       <div className="mt-2 flex flex-col gap-1">
         {chats.map((chat) => (
           <ChatCard key={chat.id} chat={chat} />
