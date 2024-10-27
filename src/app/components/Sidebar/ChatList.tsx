364fbbd28c6983e1913a5097ebf73c3f9f3f23b6
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-27 12:51:55 +0100

    Update ChatList.tsx

--- src/app/components/Sidebar/ChatList.tsx
+++ src/app/components/Sidebar/ChatList.tsx
@@ -1,19 +1,24 @@
-import { Chat, UserProfile } from "@prisma/client";
+import { getUser } from "@/utils/supabase/server";
+import { Chat, Message, UserProfile } from "@prisma/client";
 import React from "react";
 import { ChatCard } from "./ChatCard";
 
 type Props = {
-  chats: (Chat & { users: UserProfile[] })[];
+  chats: ((Chat & { users: UserProfile[] }) & { messages: Message[] })[];
 };
 
-export function ChatList({ chats }: Props) {
+export async function ChatList({ chats }: Props) {
+  const loggedInUser = await getUser();
+
   return (
     <div className="flex flex-col gap-2">
       <div className="h-px w-full rounded-full bg-hover dark:bg-[#272727]" />
       <div className="mt-2 flex flex-col gap-1">
-        {chats.map((chat) => (
-          <ChatCard key={chat.id} chat={chat} />
-        ))}
+        {chats.map((chat) => {
+          const chattingWith = chat.users.filter((user) => user.id !== loggedInUser?.id);
+
+          return <ChatCard key={chat.id} chat={chat} chattingWith={chattingWith} />;
+        })}
       </div>
     </div>
   );
