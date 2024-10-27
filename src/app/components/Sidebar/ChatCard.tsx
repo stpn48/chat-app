b597a86a6fc00a69f0341849a7d35880842f02e2
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-27 12:51:54 +0100

    Update ChatCard.tsx

--- src/app/components/Sidebar/ChatCard.tsx
+++ src/app/components/Sidebar/ChatCard.tsx
@@ -1,25 +1,32 @@
+"use client";
+
 import { UserCredentials } from "@/components/UserCredentials";
-import { getUser } from "@/utils/supabase/server";
-import { Chat, UserProfile } from "@prisma/client";
+import { Chat, Message, UserProfile } from "@prisma/client";
 import Link from "next/link";
-import React from "react";
+import React, { useCallback } from "react";
+import { useNewMsgCount } from "./hooks/useNewMsgCount";
+import { NewMessagesCount } from "./NewMessagesCount";
 
 type Props = {
-  chat: Chat & { users: UserProfile[] };
+  chat: (Chat & { users: UserProfile[] }) & { messages: Message[] };
+  chattingWith: UserProfile[];
 };
 
-export async function ChatCard({ chat }: Props) {
-  const loggedInUser = await getUser();
+export function ChatCard({ chat, chattingWith }: Props) {
+  const { newMessagesCount, setNewMessagesCount } = useNewMsgCount(chat.messages);
 
-  const chattingWith = chat.users.filter((user) => user.id !== loggedInUser?.id);
+  const resetNewMessagesCount = useCallback(() => {
+    setNewMessagesCount(0);
+  }, []);
 
   return (
-    <Link href={`/app/chat/${chat.id}`}>
+    <Link onClick={resetNewMessagesCount} href={`/app/chat/${chat.id}`}>
       <div
         key={chat.id}
-        className="flex cursor-pointer items-center gap-4 rounded-lg p-2 hover:bg-hover dark:hover:bg-dark-hover"
+        className="flex cursor-pointer items-center justify-between rounded-lg p-2 hover:bg-hover dark:hover:bg-dark-hover"
       >
         <UserCredentials user={chattingWith[0]} />
+        {newMessagesCount > 0 && <NewMessagesCount newMessagesCount={newMessagesCount} />}
       </div>
     </Link>
   );
