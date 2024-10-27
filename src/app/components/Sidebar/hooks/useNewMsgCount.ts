0e4fbf546c103d5d4b3b8d8cf1fde14f0af38562
stepan <133258815+stpn48@users.noreply.github.com>
2024-10-27 12:51:56 +0100

    Create useNewMsgCount.ts

---
+++ src/app/components/Sidebar/hooks/useNewMsgCount.ts
@@ -0,0 +1,20 @@
+"use client";
+
+import { getNewMsgCount } from "@/app/actions/getNewMsgCount";
+import { Message } from "@prisma/client";
+import { useEffect, useState } from "react";
+
+export function useNewMsgCount(messages: Message[]) {
+  const [newMessagesCount, setNewMessagesCount] = useState(0);
+
+  useEffect(() => {
+    const getCount = async () => {
+      const newMessagesCount = await getNewMsgCount(messages);
+      setNewMessagesCount(newMessagesCount);
+    };
+
+    getCount();
+  }, []);
+
+  return { newMessagesCount, setNewMessagesCount };
+}
